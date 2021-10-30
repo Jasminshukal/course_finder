1141d8287f04e00f9c9343e1606b8937c17dd10e
jasmin-shukla <mr.jasmin.shukal@gmail.com>
2021-10-30 18:40:06 +0530

    route add .

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[CourseController::class,'index'])->name('DEMO');
 
+Route::get('/Course',[CourseController::class,'index']);
+
 Route::get('Search',[CourseController::class,'search'])->name('search');
 
 Route::get('/Detail/{slug}',[CourseController::class,'detail'])->name('Detail');
